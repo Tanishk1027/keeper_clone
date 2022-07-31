bdf51132594d0073eef380786a53c1baf964f34a
Tanishk1027 <tanishkagarwal04@gmail.com>
2022-07-31 15:46:30 +0530

    commit 1478

--- src/components/Element.jsx
+++ src/components/Element.jsx
@@ -23,7 +23,7 @@ export default function Element(){
 
     useEffect(()=>{
         const fetchnotes = async()=>{
-           const res = await axios.post("/note/all",{
+           const res = await axios.post("https://keeper-clone-mern.herokuapp.com/note/all",{
                 user
             });
             console.log(res.data);
